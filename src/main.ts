2eb0b5649bb38b422b29c4818509eae4793ed70f
Marcos César Domingues <dfenixweb@gmail.com>
2024-02-27 11:54:21 -0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as winston from 'winston';
+import * as cors from 'cors';
 
 export const logger = winston.createLogger({
   level: 'info',
@@ -10,6 +11,26 @@ export const logger = winston.createLogger({
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000);
+  app.use(cors());
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+    );
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Content-Type, Authorization',
+    );
+
+    // Se a solicitação for OPTIONS, responda imediatamente com status 200 OK
+    if (req.method === 'OPTIONS') {
+      return res.status(200).end();
+    }
+
+    next();
+  });
+
+  await app.listen(3000);
 }
 bootstrap();
